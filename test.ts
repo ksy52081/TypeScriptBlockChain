803cee3e5f18d2301340f9929ec9c64df084dd66
ksy52081 <ksy52081@gmail.com>
2022-05-26 23:08:22 +0900

    [Lecture] #2.2 type xxx = {a:string, b?:number} 정의하기,물음표로 optional 정의하기  함수의 output 타입 정의하기

--- test.ts
+++ test.ts
@@ -22,3 +22,38 @@ const player = {
 
 player.age = false;
 //Type 'boolean' is not assignable to type 'number'.
+
+
+
+const player : {
+    name : string;
+    age : number;
+} = {
+    name : "SY"
+}
+
+
+const player2 : {
+    name : string;
+    age? : number;
+} = {
+    name : "SY"
+}
+
+
+
+const playerNico : {
+    name : string;
+    age? : number;
+} = {
+    name:"nico"
+}
+
+const playerSY : {
+    name : string;
+    age? : number;
+} = {
+    name:"SY",
+    age:13
+}
+// 같은 것을 반복하는것은 비효율적이다.
