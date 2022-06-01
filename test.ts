d35586f3c067d4c130523d2caf52f872d084f159
ksy52081 <ksy52081@gmail.com>
2022-06-02 02:02:13 +0900

    [Lecture]#4.0 public/private/protected차이, constructor, abstract class abstract method(직접 정의하지 않고 call signiture만 넣어준다, 정확한 구현은 상속받은 클래스에서 해야함),

--- test.ts
+++ test.ts
@@ -23,8 +23,6 @@
 // player.age = false;
 // //Type 'boolean' is not assignable to type 'number'.
 
-
-
 // const player : {
 //     name : string;
 //     age : number;
@@ -32,15 +30,12 @@
 //     name : "SY"
 // }
 
-
 // const player2 : {
 //     name : string;
 //     age? : number;
 // } = {
 //     name : "SY"
 // }
-
-
 
 // const playerNico : {
 //     name : string;
@@ -72,11 +67,11 @@
 // numlist.push(3) //readonly가 들어가면 Property 'push' does not exist on type 'readonly number[] 에러를 출력한다
 // //push filter, map 등 list를 변화시키는 모든 함수들이 비활성화된다.
 
-// Tuple 
+// Tuple
 
 // function hello(): never {
 //     return "x"
-// } 
+// }
 
 // type AddType2  = {
 //     (a:number, b:number) : number
@@ -86,26 +81,90 @@
 // const add : AddType2 = (a,b,c) =>{
 //     return a+b
 
-type Superprint = {
-    <T>(arr: T[]) : void
-    // (arr: any[]) : void
-}
-const superPrint : Superprint = (arr) => {
-    arr.forEach(i => console.log(i))
-    
+// type Superprint = {
+//     <T>(arr: T[]) : void
+//     // (arr: any[]) : void
+// }
+// const superPrint : Superprint = (arr) => {
+//     arr.forEach(i => console.log(i))
+
+// }
+
+// type FirstPrintType = {
+// <TypePlaceholder>(arr: TypePlaceholder[]) : TypePlaceholder
+// }
+// const printFirst : FirstPrintType = (arr) => arr[0]
+
+// const a = superPrint([1,2,3,4])
+// //const superPrint: <number>(arr: number[]) => void
+// const b = superPrint([true,false,false])
+// //const superPrint: <boolean>(arr: boolean[]) => void
+// const c = superPrint(["a", "b", "c", "d"])
+// //const superPrint: <string>(arr: string[]) => void
+// const d = superPrint([1,true,false,"hello"])
+// //const superPrint: <string | number | boolean>(arr: (string | number | boolean)[]) => void
+
+//#4.0 Class
+
+class Player {
+  constructor(
+    private firstName: string,
+    private lastName: string,
+    public nickName: string
+  ) {}
 }
 
-type FirstPrintType = {
-<TypePlaceholder>(arr: TypePlaceholder[]) : TypePlaceholder
+const sy = new Player("Sangyoon", "kim", "pierre");
+
+// sy.firstName
+//Property 'firstName' is private and only accessible within class 'Player'
+sy.nickName;
+
+////////////////////Abstract Class /////////////////////////
+
+abstract class User {
+  constructor(
+    private firstName: string,
+    private lastName: string,
+    public nickName: string
+  ) {}
+  //private getFullName(){ //private를 쓰면 외부에서 이 함수를 쓸 수 없다.
+  getFullName() {
+    return `$(this.firstName) $(this.lastName)`; //method의 implementation(구현)
+  }
 }
-const printFirst : FirstPrintType = (arr) => arr[0]
 
+class Player2 extends User {}
+const sy2 = new Player2("Sangyoon", "kim", "pierre");
+sy2.getFullName();
 
-const a = superPrint([1,2,3,4])
-//const superPrint: <number>(arr: number[]) => void
-const b = superPrint([true,false,false])
-//const superPrint: <boolean>(arr: boolean[]) => void
-const c = superPrint(["a", "b", "c", "d"])
-//const superPrint: <string>(arr: string[]) => void
-const d = superPrint([1,true,false,"hello"])
-//const superPrint: <string | number | boolean>(arr: (string | number | boolean)[]) => void
+//////////////Abstract Method //////////////////////////
+
+abstract class User3 {
+  constructor(
+    private firstName: string,
+    public lastName: string,
+    protected nickName: string // private nickName : string,
+  ) {}
+  abstract getfirstName(): void;
+  abstract getlastName(): void;
+  abstract getNickname(): void;
+}
+
+class Player3 extends User3 {
+  getfirstName(): void {
+    console.log(this.firstName); // private는 정의한 클래스 밖에서는 사용할 수 없다.
+  }
+  getlastName(): void {
+    console.log(this.lastName); // public은 어디에서든 쓸 수 있다.
+  }
+  getNickname(): void {
+    console.log(this.nickName); // protected는 정의한 클래스와, 상속한 클래스에서 쓸 수 있다.
+  }
+}
+
+const sy3 = new Player3("Sangyoon", "kim", "pierre");
+
+sy3.firstName; // private는 정의한 클래스 밖에서는 사용할 수 없다.
+sy3.lastName; // public은 어디에서든 사용할 수 있다.
+sy3.nickname; // 상속한 클래스 내부가 아니므로 protected 는 쓸 수 없다.
